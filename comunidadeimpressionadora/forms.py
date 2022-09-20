307f18cb1a17a066222cb30140ab92948710d1fb
sivanildosouza <110950356+sivanildooliveira@users.noreply.github.com>
2022-09-20 07:14:07 -0300

    kjb

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,18 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comunidadeimpressionadora.models import Usuarios
 
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de Usuário', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    username = StringField('Nome de Usuário', validators=[DataRequired(message='Campo Obrigatorio *')])
+    email = StringField('E-mail', validators=[DataRequired(message='Campo Obrigatorio *'), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(message='Campo Obrigatorio *'), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(message='Campo Obrigatorio *'), EqualTo('senha', message='A senha digitada não é correspodente!')])
     botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuarios.query.filter_by(email=email.data).first()
+
+        if usuario:
+            raise ValidationError(f'O e-mail "{email.data}" já está em uso.')
 
 
 class FormLogin(FlaskForm):
-    lemail = StringField('E-mail', validators=[DataRequired(), Email()])
-    lsenha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lemail = StringField('E-mail', validators=[DataRequired(message='Campo Obrigatorio *'), Email()])
+    lsenha = PasswordField('Senha', validators=[DataRequired(message='Campo Obrigatorio *'), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
--- comunidadeimpressionadora/routs.py
+++ comunidadeimpressionadora/routs.py
@@ -27,14 +27,14 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+    LC = 'l' if 'botao_submit_login' in request.form else 'c'
 
-    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        print('ok')
+    if form_login.validate_on_submit() and LC:
         flash(f'Login feito com sucesso no e-mail: {form_login.lemail.data}', 'alert-success')
         return redirect(url_for('home'))
-    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
 
 
+    if form_criarconta.validate_on_submit() and not LC:
         #criar usuario
         senha_criptografada = bcript.generate_password_hash(form_criarconta.senha.data)
         print(senha_criptografada)
@@ -46,5 +46,6 @@ def login():
 
 
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
-        return redirect(url_for('home'))
+        return redirect(url_for('home'), LC=LC)
+
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
