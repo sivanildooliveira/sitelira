38f84874e20ea62eb9fba5cd84d3290760f642f8
sivanildosouza <110950356+sivanildooliveira@users.noreply.github.com>
2022-09-28 06:58:30 -0300

    at aula 27

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
-
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
-bcript = Bcrypt(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
-
-from comunidadeimpressionadora import routs
+from comunidadeimpressionadora import routes
--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,25 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from comunidadeimpressionadora.models import Usuarios
+from comunidadeimpressionadora.models import Usuario
 
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de Usuário', validators=[DataRequired(message='Campo Obrigatorio *')])
-    email = StringField('E-mail', validators=[DataRequired(message='Campo Obrigatorio *'), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(message='Campo Obrigatorio *'), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(message='Campo Obrigatorio *'), EqualTo('senha', message='A senha digitada não é correspodente!')])
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
     def validate_email(self, email):
-        usuario = Usuarios.query.filter_by(email=email.data).first()
-
+        usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            raise ValidationError(f'O e-mail "{email.data}" já está em uso.')
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
 
 
 class FormLogin(FlaskForm):
-    lemail = StringField('E-mail', validators=[DataRequired(message='Campo Obrigatorio *'), Email()])
-    lsenha = PasswordField('Senha', validators=[DataRequired(message='Campo Obrigatorio *'), Length(6, 20)])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
