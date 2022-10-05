dbdb1ddea8773e2471c7a110cbd97c622a9be1a4
sivanildosouza <110950356+sivanildooliveira@users.noreply.github.com>
2022-10-04 21:30:22 -0300

    redirect apos login

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from comunidadeimpressionadora.models import Usuario
+from comunidadeimpressionadora.models import Usuarios
 
 
 class FormCriarConta(FlaskForm):
@@ -10,15 +10,32 @@ class FormCriarConta(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
-
+    
     def validate_email(self, email):
-        usuario = Usuario.query.filter_by(email=email.data).first()
-        if usuario:
-            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+    	usuario = Usuarios.query.filter_by(email=email.data).first()
+    	if usuario:
+    		raise ValidationError('E-mail já Cadastrado! tente outro E-mail.')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lemail = StringField('E-mail', validators=[DataRequired(), Email()])
+    lsenha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
+    
+    #def validate_lemail(self, lemail):
+#    	usuario = Usuarios.query.filter_by(email=lemail.data).first()
+#    	if not usuario:
+#    		raise ValidationError('E-mail não Cadastrado! verifique seu e-mail ou Crie um conta para continuar.')
+#    	
+#    def validate_lsenha(self, lsenha):
+#    	
+#    	d_senha = Usuarios.query.filter_by(email=self.lemail.data).first()
+#    	try:
+#    		senha_user = d_senha.senha
+#    	except:
+#    		return 
+#    	
+#    	if senha_user != lsenha.data:
+#    		raise ValidationError('Senha incorreta!')
+    	
---
+++ comunidadeimpressionadora/routs.py
@@ -0,0 +1,99 @@
+
+from flask import render_template, redirect, url_for, request, flash
+from comunidadeimpressionadora import app, database, bcrypt
+from comunidadeimpressionadora.forms import FormCriarConta, FormLogin
+from comunidadeimpressionadora.models import Usuarios, Post
+from flask_login import login_user, logout_user, current_user, login_required
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
+ 
+
+@app.route('/contato')
+@login_required
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/usuarios')
+@login_required
+def usuarios():
+    lista_usuarios = Usuarios.query.all()
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        usuario = Usuarios.query.filter_by(email=form_login.lemail.data).first()
+        
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.lsenha.data):
+
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+	        
+            flash(f'Login feito com sucesso no e-mail: {form_login.lemail.data}', 'alert-success')
+	        
+            par_next = request.args.get('next')
+            print(par_next)
+
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
+	       	 
+        else:
+            flash(f'Falha no login! E-mail ou senha não confere. Tente novamente.', 'alert-danger')
+
+
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        
+        par_next = request.args.get('next')
+        
+        #criar usuario
+        senha_criptografada = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        
+        print(senha_criptografada)
+        usuario = Usuarios(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_criptografada)
+        
+        #adicionar usuario a sessao
+        database.session.add(usuario)
+        
+        #comitar no banco de dados
+        database.session.commit()
+
+
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+
+        usuario = Usuarios.query.filter_by(email=form_criarconta.email.data).first()
+        login_user(usuario)
+
+        if par_next:
+            return redirect(par_next)
+        else:
+            return redirect(url_for('home'))
+        
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+    
+
+@app.route('/sair')
+@login_required
+def sair():
+	logout_user()
+	flash('Logout feito com sucesso', 'alert-success')
+	return redirect(url_for('home'))
+
+@app.route('/perfil')
+@login_required
+def perfil():
+	return render_template('perfil.html')
+
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+	return render_template('criarpost.html')
