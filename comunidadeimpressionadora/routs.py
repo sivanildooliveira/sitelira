04a14fb5f93d4310cccb6f22f7e503a2322086d5
sivanildosouza <110950356+sivanildooliveira@users.noreply.github.com>
2022-10-27 01:16:59 -0300

    home page

--- comunidadeimpressionadora/routs.py
+++ comunidadeimpressionadora/routs.py
@@ -12,7 +12,8 @@ from comunidadeimpressionadora.functionss import salvar_imagem, salvar_bg_imagem
 @app.route('/')
 @login_required
 def home():
-    posts = Post.query.all()
+    posts = Post.query.order_by(Post.id.desc())
+    
     return render_template('home.html', posts=posts, avatar=retur_foto_perfil)
  
 
